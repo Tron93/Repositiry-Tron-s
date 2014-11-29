7298f02f4c63e782f8cebc84c8b8301435c221ac
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-11-29 19:07:11 +0300

    Adding some array tasks

---
+++ 29-11-2014/29-11-2014/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+
+namespace _29_11_2014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            TaskOne.Main();
+            TaskTwo.Main();
+            Console.ReadKey();
+        }
+    }
+}
