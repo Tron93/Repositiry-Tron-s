97f550549e8e97090a401e4bab7d3492b499b80d
Tsimafei Markhel <tsimafei.markhel@gmail.com>
2014-12-13 18:52:06 +0300

    Added work done since 07-12-2014

---
+++ 13-12-2014/13-12-2014/CalcDate.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_12_2014
+{
+    public enum Month
+    {
+        Jan,
+        Feb,
+        Mar,
+        Apr,
+        May,
+        Jun,
+        Jul,
+        Aug,
+        Sep,
+        Oct,
+        Nov,
+        Dec
+    }
+
+    public class MonthDay
+    {
+        public Month Mon { get; private set; }
+        public int Day { get; private set; }
+
+        public MonthDay(Month mon, int day)
+        {
+            Mon = mon;
+            Day = day;
+        }
+    }
+
+    public class CalcDate
+    {
+        private static readonly IEnumerable<int> ListOfDay = new List<int> { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        public void Main()
+        {
+            int dayNumber = int.Parse(Console.ReadLine());
+            MonthDay mon = GetMonthAndDay(dayNumber);
+            Console.WriteLine("Месяц {0} день {1}", mon.Mon.ToString(), mon.Day.ToString());
+        }
+
+        public static MonthDay GetMonthAndDay(int dayNumber)
+        {
+            int monthNumber = 0;
+            int dayCounter = dayNumber;
+            foreach (int numberOfDays in ListOfDay)
+            {
+                if (dayCounter > numberOfDays)
+                {
+                    dayCounter -= numberOfDays;
+                    monthNumber++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new MonthDay((Month)monthNumber, dayCounter);
+        }
+    }
+}
---
+++ 13-12-2014/13-12-2014/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_12_2014
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcDate cd = new CalcDate();
+            cd.Main();
+        }
+    }
+}
